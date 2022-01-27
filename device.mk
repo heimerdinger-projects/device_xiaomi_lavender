b41c94ced9cc00e873f3348d883e685c7e0d5478
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    lavender: Drop support for Snapdragon Camera
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
     libstdc++.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0:64
 
 PRODUCT_COPY_FILES += \
