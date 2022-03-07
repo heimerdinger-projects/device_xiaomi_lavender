9c4e4bb9f783c97247486271c64ca6922d641b08
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-03-07 11:10:09 +0000

    lavender: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    https://github.com/PixelExp-Project/vendor_aosp/commit/c3b9f5c170c794966bb941282c3f9f18baa6279a
    
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Call recording
+PRODUCT_PACKAGES += \
+    com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
