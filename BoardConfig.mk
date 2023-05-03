a3f2a113ebdda6640d25b147bd0fa3b97aecbf8a
clarencelol <clarencekuiek@icloud.com>
2023-05-03 11:51:03 +0800

    lavender: Fully switched to jemalloc
    * https://review.arrowos.net/c/ArrowOS/android_build/+/18089
    
    Change-Id: I7576a69c2e181b5fa39cda70393b12828bd304b5
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,10 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lavender
 TARGET_RECOVERY_DEVICE_MODULES := libinit_lavender
 
+# Jemalloc
+MALLOC_SVELTE := true
+MALLOC_SVELTE_FOR_LIBC32 := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_power_aware=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7
