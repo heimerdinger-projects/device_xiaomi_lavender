a383594dc82e214ea946fdf5505cd01df56189cd
SebaUbuntu <seba@sebaubuntu.dev>
2023-11-22 20:15:03 -0300

    lavender: Drop product_launched_with_p.mk
    Change-Id: I08267a0a277fc36e231bebcfc68b964b4aac4661

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -7,7 +7,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -25,4 +24,3 @@ PRODUCT_MODEL := Redmi Note 7
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := lavender
-
