063a77d869015d481fe65f611b1d161fcb6dc53d
Bruno Martins <bgcngm@gmail.com>
2023-12-01 12:37:40 -0300

    lavender: Import XiaomiDoze for Android 13
    Change-Id: I4a4e98b3f4f6e6e3b0778a204c22e7b61851d7be

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework
