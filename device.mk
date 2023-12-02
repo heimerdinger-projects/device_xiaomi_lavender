404c18cc5200a1f7b39cf2d439b7ba43ac957a45
Vhmit <viktor.9630@hotmail.com>
2023-12-02 07:15:23 -0300

    lavender: Import Perf blobs from S62Pro/RKQ1.210406.002
    * Perf-configs from LA.UM.9.2.1.r1-08700-sdm660
    
    Co-authored-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I9aad56de47976acf3b20606214b5ed68c2f2e6e4
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- device.mk
+++ device.mk
@@ -270,7 +270,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
+    android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
