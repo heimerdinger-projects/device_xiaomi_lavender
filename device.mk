4a0f8042746c48135a3207ea9fa80fac6b4412a0
vivekachooz <vivekachooz@gmail.com>
2023-12-02 10:24:27 -0300

    lavender: Import Perf blobs from LA.UM.9.2.r1-03500-SDMxx0.0
    * Blobs BSP from SLD_sprout 11-RKQ1.210607.001-00WW_3_490-release-keys
    
    Change-Id: I59843a27d271a98706655f7652fd62ecac0d32f3

--- device.mk
+++ device.mk
@@ -270,8 +270,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
