a2f952fa5ac0d3153272d93e4687090f814de972
Vhmit <viktor.9630@hotmail.com>
2023-12-04 00:15:03 -0300

    lavender: Drop mock hidl power stats
    * Log: E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData
    
    Change-Id: Id4570e154e3c0313d107e81780511f9846d6e12f
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- device.mk
+++ device.mk
@@ -274,8 +274,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service
 
 # Protobuf
 PRODUCT_PACKAGES += \
