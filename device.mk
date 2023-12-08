d8a28f53584d066611dc1f70fd5f0aed26450705
Vhmit <viktor.9630@hotmail.com>
2023-12-08 20:01:01 -0300

    lavender: Rebrand device tree to LineageOS based
    Change-Id: I9ee8aa20a17699232f2241792d0267a6f518664c

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
