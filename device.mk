2cbec7ff41c579f7c58ce7fafb9657cfbd92a386
orges <me@orgesified.ml>
2023-12-22 10:39:53 -0300

    lavender: configs: Import display config
    * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
    * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I7a7445814f7f5000508e337202db5a55a12d088c

--- device.mk
+++ device.mk
@@ -118,6 +118,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
