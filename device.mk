f6dea4b08b42d8c3097731f5426d8e78dc0d8e13
Vhmit <viktor.9630@hotmail.com>
2024-01-01 11:48:10 -0300

    lavender: Rebrand rro_overlays to resource
    * Initial migration of overlays to resource
    
    Change-Id: Ia351ddce062363ce800b56f790cab4e00d291a00
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- device.mk
+++ device.mk
@@ -7,11 +7,12 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/resource-overlay \
     $(LOCAL_PATH)/overlay-arrow
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay \
-    NotchBarKiller
+    NotchBarKiller \
+    WifiOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -424,7 +425,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
