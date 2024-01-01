7ce1fe282e4cbd0cabfac5b59246d6c8dd9528e2
SebaUbuntu <seba@sebaubuntu.dev>
2023-12-31 22:13:58 -0300

    lavender: Move rootdir to blueprint
    Change-Id: I68e8519e4096e849acc234e037aa718134eba729

--- device.mk
+++ device.mk
@@ -322,19 +322,21 @@ PRODUCT_COPY_FILES += \
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.class_main.sh \
     init.device.rc \
     init.lavender.perf.rc \
     init.msm.usb.configfs.rc \
-    init.qcom.early_boot.sh \
-    init.qcom.post_boot.sh \
     init.qcom.rc \
-    init.qcom.sh \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.usb.sh
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
