40371f11c88f3bc946d8a7bcd395d7a1355523ec
Vhmit <viktor.9630@hotmail.com>
2024-01-12 10:57:42 -0300

    lavender: Re-organize packages to hidl/prebuilt
    Change-Id: I1e8d66a902f5b2cad9d926b155ca208e1a50fc3d
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,9 +76,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lavender
