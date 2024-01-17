dc1471f6a98ccd4745d25b2bb7cdba29594a61f2
klozz <carlosj@klozz.dev>
2024-01-16 23:25:22 -0300

    lavender: Flash dummy super image if needed
    * * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used.
    
    Change-Id: I08424b9e2db8deea9da4f50e30d1b96f587d0f76
    Signed-off-by: klozz <carlosj@klozz.dev>

--- releasetools.py
+++ releasetools.py
@@ -14,6 +14,15 @@
 # limitations under the License.
 
 import common
+import re
+
+def FullOTA_InstallBegin(info):
+  AddImage(info, "super_dummy.img", "/tmp/super_dummy.img");
+  flash_script = open("device/xiaomi/lavender/dynamic_partitions/flash_super_dummy.sh", 'r').read()
+  common.ZipWriteStr(info.output_zip, "install/bin/flash_super_dummy.sh", flash_script);
+  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
+  info.script.AppendExtra('run_program("/sbin/sh", "/tmp/flash_super_dummy.sh");')
+  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
